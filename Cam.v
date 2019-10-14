d19e29139beded125836e78aba4cd2d5876e1021
Murali Vijayaraghavan <vmurali@sifive.com>
2019-10-14 16:00:53 -0700

    fixed stubs for Cam

--- Cam.v
+++ Cam.v
@@ -1,7 +1,6 @@
 (* Content Addressable Memory *)
-Require Import Wf.
-Require Import Wf_nat.
 Require Import Kami.AllNotations.
+Require Import StdLibKami.PseudoLru.
 
 Axiom cheat : forall A, A.
 
@@ -10,41 +9,55 @@ Section cam.
   Open Scope kami_action.
 
   Variable Tag Data ReadCtxt ClearCtxt : Kind.
-  Variable regNames : list string.
+  Variable matchRead: forall ty, Tag @# ty -> ReadCtxt @# ty -> Tag @# ty -> Bool @# ty.
+  Variable  matchClear: forall ty, Tag @# ty -> ClearCtxt @# ty -> Tag @# ty -> Bool @# ty.
 
   Record Cam
     := {
-      matchRead: forall ty, Tag @# ty -> ReadCtxt @# ty -> Tag @# ty -> Bool @# ty;
+        camRead:
+          forall (ty : Kind -> Type)
+                 (tag : Tag @# ty)
+                 (ctxt : ReadCtxt @# ty),
+            ActionT ty (Maybe Data);
+        
+        camWrite:
+          forall (ty : Kind -> Type)
+                 (tag : Tag @# ty)
+                 (data : Data @# ty),
+            ActionT ty Void;
+        
+      camFlush: forall ty, ActionT ty Void;
 
-      matchClear: forall ty, Tag @# ty -> ClearCtxt @# ty -> Tag @# ty -> Bool @# ty;
-
-      camRead
-        (ty : Kind -> Type)
-        (tag : Tag @# ty)
-        (ctxt : ReadCtxt @# ty)
-        : ActionT ty (Maybe Data)
-        := utila_acts_find_pkt
-             (map
-               (fun regName : string
-                 => Read entry : Maybe (Pair Tag Data) <- regName;
-                    utila_acts_opt_pkt
-                      (#entry @% "data" @% "snd")
-                      (#entry @% "valid" &&
-                       matchRead tag ctxt (#entry @% "data" @% "fst")))
-                regNames);
-
-      camWrite
-        (ty : Kind -> Type)
-        (tag : Tag @# ty)
-        (data : Data @# ty)
-        :  ActionT ty Void
-        := cheat _;
-
-      camFlush: forall ty, ActionT ty Void := cheat _;
-
-      camClear: forall ty, Tag @# ty -> ClearCtxt @# ty -> ActionT ty Void := cheat _
+      camClear: forall ty, Tag @# ty -> ClearCtxt @# ty -> ActionT ty Void;
     }.
 
+  Section SimpleCam.
+    Variable regName : string.
+    Variable size : nat.
+
+    Definition SimpleCam : Cam
+      := {| camRead :=
+              fun ty tag ctxt =>
+                utila_acts_find_pkt
+                  (map
+                     (fun i: nat
+                      => Read entry : Maybe (Pair Tag Data) <- regName ++ "__" ++ natToHexStr i;
+                           utila_acts_opt_pkt
+                             (#entry @% "data" @% "snd")
+                             (#entry @% "valid" &&
+                               matchRead tag ctxt (#entry @% "data" @% "fst")))
+                     (seq 0 size));
+            
+            camWrite :=
+              fun ty tag data => cheat _ ;
+            
+            camFlush :=
+              fun ty => cheat _;
+
+            camClear :=
+              fun ty tag ctxt => cheat _ |}.
+  End SimpleCam.
+    
   Close Scope kami_action.
   Close Scope kami_expr.
 
