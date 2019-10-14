5aaf5799479cdd8af4548f65fac6c6b121cd17a5
Larry Lee <llee454@gmail.com>
2019-10-14 13:11:36 -0700

    Revised Cam.

--- Cam.v
+++ Cam.v
@@ -1,4 +1,6 @@
 (* Content Addressable Memory *)
+Require Import Wf.
+Require Import Wf_nat.
 Require Import Kami.AllNotations.
 
 Axiom cheat : forall A, A.
@@ -7,20 +9,40 @@ Section cam.
   Open Scope kami_expr.
   Open Scope kami_action.
 
-  Variable ty: Kind -> Type.
-  Variable Tag Data ReadCtxt ClearCtxt: Kind.
+  Class Cam
+    (Tag Data ReadCtxt ClearCtxt : Kind)
+    (regNames : list string)
+    := {
+      matchRead: forall ty, Tag @# ty -> ReadCtxt @# ty -> Tag @# ty -> Bool @# ty;
 
-  Variable matchRead: Tag @# ty -> ReadCtxt @# ty -> Tag @# ty -> Data @# ty -> Bool @# ty.
+      matchClear: forall ty, Tag @# ty -> ClearCtxt @# ty -> Tag @# ty -> Bool @# ty;
 
-  Variable matchClear: Tag @# ty -> ClearCtxt @# ty -> Tag @# ty -> Data @# ty -> Bool @# ty.
+      camRead
+        (ty : Kind -> Type)
+        (tag : Tag @# ty)
+        (ctxt : ReadCtxt @# ty)
+        : ActionT ty (Maybe Data)
+        := utila_acts_find_pkt
+             (map
+               (fun regName : string
+                 => Read entry : Maybe (Pair Tag Data) <- regName;
+                    utila_acts_opt_pkt
+                      (#entry @% "data" @% "snd")
+                      (#entry @% "valid" &&
+                       matchRead tag ctxt (#entry @% "data" @% "fst")))
+                regNames);
 
-  Definition camRead: Tag @# ty -> ReadCtxt @# ty -> ActionT ty (Maybe Data) := cheat _.
+      camWrite
+        (ty : Kind -> Type)
+        (tag : Tag @# ty)
+        (data : Data @# ty)
+        :  ActionT ty Void
+        := cheat _;
 
-  Definition camWrite: Tag @# ty -> Data @# ty -> ActionT ty Void := cheat _.
+      camFlush: forall ty, ActionT ty Void := cheat _;
 
-  Definition camFlush: ActionT ty Void := cheat _.
-
-  Definition camClear: Tag @# ty -> ClearCtxt @# ty -> ActionT ty Void := cheat _.
+      camClear: forall ty, Tag @# ty -> ClearCtxt @# ty -> ActionT ty Void := cheat _;
+    }.
 
   Close Scope kami_action.
   Close Scope kami_expr.
