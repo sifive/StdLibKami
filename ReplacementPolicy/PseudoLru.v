c97fd2ca681754f71f8cb235be63d88ab2642790
Murali Vijayaraghavan <vmurali@sifive.com>
2019-10-15 23:42:24 -0700

    bumping StdLibKami

--- ReplacementPolicy/PseudoLru.v
+++ ReplacementPolicy/PseudoLru.v
@@ -3,247 +3,159 @@ Require Import Kami.AllNotations.
 Require Import StdLibKami.ReplacementPolicy.Ifc.
 
 Section lru.
-  Open Scope kami_expr.
-  Open Scope kami_action.
+  Class PseudoLruParams := { stateRegName : string;
+                             num : nat
+                           }.
 
-  (*
-    the number of data registers.
-    Note: should be of the form 2^n for some n.
-  *)
-  Variable num : nat.
-
-  (* the name of the register that stores the path of the least recently used data register *)
-  Variable stateRegName : string.
-
-  (*
-    Note: the number of internal nodes in the balanced binary tree is [num - 1].
-  *)
-  Definition stateWidth := (num - 1)%nat.
-
-  (* represents the state of the Lru unit - i.e. the value of each node within the state tree. *)
-  Definition State := Array stateWidth Bool.
-
-  (*
-    Note: the total number nodes and leaves in the tree is [2*num - 1].
-  *)
-  Definition treeNodeWidth := Nat.log2_up (2 * num - 1)%nat.
-
-  Definition TreeNodeIndex := Bit treeNodeWidth.
-
+  Variable params: PseudoLruParams.
+  
+  Definition State := Array (num - 1) Bool.
   Definition IndexWidth := Nat.log2_up num.
-
   Definition Index := Bit IndexWidth.
-
+  Definition PathWidth := IndexWidth.
+  Definition Path := Index.
+  Definition TreeIndexWidth := IndexWidth.
+  Definition TreeIndex := Index.
+  Definition Depth := IndexWidth.
+    
   Section ty.
     Variable ty : Kind -> Type.
+    
+    Definition leftOverNum := num - Nat.pow 2 (Nat.log2 num).
+    Definition twiceLeftOverNum := 2*leftOverNum.
 
-    Definition treeNodeIndex
-      (treeNode : TreeNodeIndex @# ty)
-      :  Index @# ty
-      := ~(ZeroExtendTruncLsb IndexWidth treeNode + $1).
+    Local Open Scope kami_expr.
+    Local Open Scope kami_action.
 
-    Fixpoint getVictimAux
-      (depth : nat)
-      (state : State @# ty)
-      (treeNode : TreeNodeIndex @# ty)
-      :  Index ## ty
-      := LETC index : Index <- treeNodeIndex treeNode;
-         LETC direction : Bool <- state@[#index];
-         match depth with
-         | 0
-           => RetE #index
-         | S depth'
-           => LETE result
-                :  Index
-                <- getVictimAux depth' state
-                     (IF #direction
-                       then (treeNode << ($1 : Bit 1 @# ty)) | $1 (* left child's treeNode *)
-                       else (treeNode << ($1 : Bit 1 @# ty)));    (* right child's treeNode *)
-              RetE
-                (IF #index >= $(num - 1)
-                  then #index
-                  else #result)
-         end.
+    Definition mul2 n (v: Bit n @# ty) := (v << ($1: Bit 1 @# ty)).
+    Definition div2 n (v: Bit n @# ty) := (v >> ($1: Bit 1 @# ty)). 
+    Definition mod2 n (v: Bit n @# ty) := (v & $1).
 
-    (* The maximum depth of the balanced tree. *)
-    Definition Depth := Nat.log2_up num.
+    Definition getPathFromIndex (i: Index @# ty) := match leftOverNum with
+                                                    | 0 => i
+                                                    | _ => (IF i < $twiceLeftOverNum
+                                                            then i
+                                                            else mul2 (i - $leftOverNum))
+                                                    end.
 
-    (* The root node treeNode. *)
-    Definition rootTreeNodeIndex : TreeNodeIndex @# ty := $$(wones treeNodeWidth) << ($1 : Bit 1 @# ty).
+    Definition getIndexFromPath (p: Path @# ty) := match leftOverNum with
+                                                   | 0 => p
+                                                   | _ => (IF p < $twiceLeftOverNum
+                                                           then p
+                                                           else div2 p + $leftOverNum)
+                                                   end.
 
-    (* Returns the index of the least recently used register *)
-    Definition getVictim'
-      :  ActionT ty Index
-      := Read state : State <- stateRegName;
-         convertLetExprSyntax_ActionT
-           (getVictimAux Depth #state rootTreeNodeIndex).
+    Section state.
+      Variable state: State @# ty.
+      Fixpoint getVictimAux (depth: nat) (i: TreeIndex @# ty) (p: Path @# ty): Path ## ty :=
+        match depth with
+        | 0 => RetE p
+        | S m => (IfE i >= $(num-1)
+                  then RetE (mul2 p)
+                  else (LETC direction : Bool <- state@[i];
+                          LETC newIndex : TreeIndex <- mul2 i + IF #direction then $2 else $1;
+                          LETC newPath : Path <- mul2 p | IF #direction then $1 else $0;
+                          LETE retPath : Path <- (getVictimAux m #newIndex #newPath);
+                          RetE #retPath) as retPath;
+                    RetE #retPath)
+        end.
+    End state.
 
-    Definition indexTreeNodeIndex
-      (index : Index @# ty)
-      :  TreeNodeIndex @# ty
-      := (~ (ZeroExtendTruncLsb treeNodeWidth index)) - $1.
-
-    (* Note: treeNode must not be the root or the dual of the root - i.e. treeNodeIndex treeNode != 0 *)
-    Definition parentTreeNodeIndex
-      (treeNode : TreeNodeIndex @# ty)
-      :  TreeNodeIndex @# ty
-      := OneExtendTruncLsb treeNodeWidth
-           (ZeroExtendTruncMsb (treeNodeWidth - 1) treeNode).
-
-    Fixpoint accessAuxRec
-      (maxDepth : nat)
-      (state : State @# ty)
-      (treeNode : TreeNodeIndex @# ty)
-      (direction : Bool @# ty)
-      :  State ## ty
-      := match maxDepth with
-         | 0 => RetE state (* impossible case - bounding recursion. *)
-         | S depth
-           => LETC index : Index <- treeNodeIndex treeNode;
-              LETC nextState : State <- state@[#index <- direction];
-              LETE result : State
-                <- accessAuxRec depth #nextState (parentTreeNodeIndex treeNode)
-                     (ZeroExtendTruncLsb 1 treeNode == $0);
-              RetE
-                (IF treeNode == rootTreeNodeIndex
-                  then #nextState
-                  else #result)
-         end.
-
-    Definition accessAux
-      (state : State @# ty)
-      (index : Index @# ty)
-      :  State ## ty
-      := LETC rawTreeNodeIndex : TreeNodeIndex <- indexTreeNodeIndex index;
-         LETC treeNode : TreeNodeIndex
-           <- IF #rawTreeNodeIndex == rootTreeNodeIndex
-               then #rawTreeNodeIndex << ($1 : Bit 1 @# ty) >> ($1 : Bit 1 @# ty) (* set msb to 0 *)
-               else #rawTreeNodeIndex;
-         accessAuxRec Depth state
-           (parentTreeNodeIndex #treeNode)
-           (ZeroExtendTruncLsb 1 #treeNode == $0).
-
-    (* updates the least recently used data to point away from the given register. *)
-    Definition access'
-      (index : Index @# ty)
-      :  ActionT ty Void
-      := Read state : State <- stateRegName;
-         LETA result : State <- convertLetExprSyntax_ActionT (accessAux #state index);
-         Write stateRegName : State <- #result;
-         Retv.
-
+    Section path.
+      Variable path: Path @# ty.
+      Local Definition pathToArray: Array PathWidth Bool @# ty := unpack (Array PathWidth Bool) match eq_sym (Nat.mul_1_r PathWidth) in _ = Y return Bit Y @# ty with
+                                                                                                | eq_refl => path
+                                                                                                end.
+      Fixpoint accessAux (depth: nat) (i: TreeIndex @# ty) (s: State @# ty): State ## ty :=
+        match depth with
+        | 0 => RetE s
+        | S m => (IfE i >= $(num-1)
+                  then RetE s
+                  else (LETC direction: Bool <- (* pathToArray@[$$ (natToWord (Nat.log2_up PathWidth) m)] *) ((path >> ($m: Bit PathWidth @# ty)) & $1) == $1;
+                          LETC newIndex : TreeIndex <- mul2 i + (IF #direction then $2 else $1);
+                          LETC newState : State <- s@[i <- !#direction];
+                          LETE retState : State <- (accessAux m #newIndex #newState);
+                          RetE #retState) as retState;
+                    RetE #retState)
+        end.
+    End path.
   End ty.
+  
+  Local Open Scope kami_expr.
+  Local Open Scope kami_action.
+  
+  Definition PseudoLru
+    :  ReplacementPolicy num
+    := {|
+        getVictim := (fun ty =>
+                        Read state : State <- stateRegName;
+                          LETA path : Path <- convertLetExprSyntax_ActionT
+                                           (getVictimAux #state Depth $0 $0);
+                          Ret (getIndexFromPath #path));
+        access := (fun ty index =>
+                     Read state : State <- stateRegName;
+                       LETA result : State <- convertLetExprSyntax_ActionT (accessAux (getPathFromIndex index) Depth $0 #state);
+                       Write stateRegName : State <- #result;
+                       Retv)
+      |}.
 
-  Close Scope kami_action.
-  Close Scope kami_expr.
+  Local Close Scope kami_action.
+  Local Close Scope kami_expr.
 End lru.
-
-Section interface.
-  Open Scope kami_expr.
-  Open Scope kami_action.
-
-  Class PseudoLruParams
-    := {
-      stateRegName : string;
-      num : nat
-    }.
-
-  Section instance.
-    Context `{psuedoLruParams : PseudoLruParams}.
-
-    Definition PsuedoLru
-      :  ReplacementPolicy num
-      := {|
-           getVictim := fun ty => getVictim' num stateRegName ty;
-           access := fun ty index => access' stateRegName index
-         |}.
-
-  End instance.
-
-  Close Scope kami_action.
-  Close Scope kami_expr.
-End interface.
 
 Section tests.
 
+  Transparent wlt_dec.
   Open Scope kami_expr.
 
-  Local Definition T := Const type true.
-  Local Definition F := Const type false.
+  Local Notation L := (Const type false).
+  Local Notation R := (Const type true).
 
-  Definition testGetVictim
-    (num : nat)
-    (state : State num @# type)
-    (expected : word (IndexWidth num))
-    :  Prop
-    := (evalLetExpr
-         (LETE result
-           : Index num
-           <- getVictimAux
-                (Depth num)
-                state
-                (rootTreeNodeIndex num type);
-          RetE (Var type (SyntaxKind _) result))) =
-        expected.
+  Definition testGetVictim (num : nat)
+    := let params := {| num := num; stateRegName := "test" |} in
+       fun (state: State params @# type) (expected : word (IndexWidth params)) =>
+         (evalLetExpr (LETE path : Path params <- getVictimAux state (Depth params) $0 $0;
+                         RetE (getIndexFromPath (Var _ (SyntaxKind _) path))) = expected).
 
-  Goal testGetVictim (num := 3) (ARRAY {T; T}) $3. Proof ltac:(reflexivity). 
-  Goal testGetVictim (num := 3) (ARRAY {T; F}) $0. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 3) (ARRAY {F; T}) $2. Proof ltac:(reflexivity).
+  Goal testGetVictim (num := 3) (ARRAY {R; R}) $2. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 3) (ARRAY {R; L}) $2. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 3) (ARRAY {L; R}) $1. Proof. reflexivity. Qed.
 
-  Goal testGetVictim (num := 6) (ARRAY {T; T; T; T; T}) $7. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 6) (ARRAY {F; T; T; T; T}) $5. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 6) (ARRAY {F; T; F; T; T}) $6. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 6) (ARRAY {T; F; T; T; F}) $2. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 6) (ARRAY {T; F; T; T; T}) $1. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 6) (ARRAY {T; T; T; F; T}) $0. Proof ltac:(reflexivity).
-
-  Goal testGetVictim (num := 7) (ARRAY {T; T; T; T; T; T}) $7. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 7) (ARRAY {T; T; T; F; T; T}) $0. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 7) (ARRAY {T; F; F; T; T; T}) $1. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 7) (ARRAY {T; F; T; T; F; T}) $2. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 7) (ARRAY {F; T; T; T; T; T}) $3. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 7) (ARRAY {F; T; T; T; T; F}) $4. Proof ltac:(reflexivity).
-  Goal testGetVictim (num := 7) (ARRAY {F; T; F; T; T; T}) $6. Proof ltac:(reflexivity).
-
-  Goal (evalExpr (indexTreeNodeIndex (num := 5) (Const type (natToWord _ 7)))) = natToWord _ 7. Proof. reflexivity. Qed.
-  Goal (evalExpr (indexTreeNodeIndex (num := 5) (Const type (natToWord _ 4)))) = 4'b"1010". Proof. reflexivity. Qed.
-  Goal (evalExpr (parentTreeNodeIndex (num := 5) (Const type (4'b"1010")))) = 4'b"1101". Proof. reflexivity. Qed.
-  Goal (evalExpr (parentTreeNodeIndex (num := 5) (Const type (4'b"1001")))) = 4'b"1100". Proof. reflexivity. Qed.
-  Goal (evalExpr (parentTreeNodeIndex (num := 5) (Const type (4'b"0110")))) = 4'b"1011". Proof. reflexivity. Qed.
-  Goal (evalExpr (parentTreeNodeIndex (num := 5) (Const type (4'b"0111")))) = 4'b"1011". Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 6) (ARRAY {R; R; R; R; R}) $5. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 6) (ARRAY {L; R; R; R; R}) $3. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 6) (ARRAY {L; R; L; R; R}) $3. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 6) (ARRAY {R; L; R; R; L}) $5. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 6) (ARRAY {R; L; R; R; R}) $5. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 6) (ARRAY {R; R; R; L; R}) $5. Proof. reflexivity. Qed.
+  
+  Goal testGetVictim (num := 7) (ARRAY {R; R; R; R; R; R}) $6. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 7) (ARRAY {R; R; R; L; R; R}) $6. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 7) (ARRAY {R; L; L; R; R; R}) $5. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 7) (ARRAY {R; L; R; R; L; R}) $6. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 7) (ARRAY {L; R; R; R; R; R}) $3. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 7) (ARRAY {L; R; R; R; R; L}) $3. Proof. reflexivity. Qed.
+  Goal testGetVictim (num := 7) (ARRAY {L; R; L; R; R; R}) $3. Proof. reflexivity. Qed.
 
   Definition evalArray
     (k : Kind)
     (n : nat)
-    :  Array n k ## type -> list (type k)
-    := match n return Array n k ## type -> list (type k) with
-       | 0 => fun _ => []
-       | S n
-         => nat_rect
-              (fun m : nat
-                => m < (S n) -> Array (S n) k ## type -> list (type k))
-              (fun (H : 0 < (S n)) xs
-                => [(evalLetExpr xs) (Fin.of_nat_lt H)])
-              (fun m F (H : S m < (S n)) xs
-                => (F (Nat.lt_succ_l m (S n) H) xs) ++
-                   [(evalLetExpr xs) (Fin.of_nat_lt H)])
-              n (Nat.lt_succ_diag_r n)
-      end%nat.
+    (arr: Array n k ## type)
+    :  list (type k)
+    := map (evalLetExpr arr) (getFins n).
 
-  Definition testAccess
-    (num : nat)
-    (state : State num @# type)
-    (index : Index num @# type)
-    (expected : list bool)
-    :  Prop
-    := evalArray (accessAux (num := num) state index) = expected.
+  Definition testAccess (num : nat) :=
+    let params := {| num := num; stateRegName := "test" |} in
+    fun (state: State params @# type) (index: Index params @# type) (expected : list bool) =>
+      evalArray (accessAux (getPathFromIndex index) (Depth params) $0 state) = expected.
 
-  Goal testAccess (num := 2) (ARRAY {T}) (Const type (1'b"0")) [true]. Proof ltac:(reflexivity).
-  Goal testAccess (num := 2) (ARRAY {T}) (Const type (1'b"1")) [false]. Proof ltac:(reflexivity).
-  Goal testAccess (num := 3) (ARRAY {T; T}) (Const type (2'b"11")) [false; false]. Proof ltac:(reflexivity).
-  Goal testAccess (num := 3) (ARRAY {T; T}) (Const type (2'b"00")) [false; true]. Proof ltac:(reflexivity).
-  Goal testAccess (num := 3) (ARRAY {T; T}) (Const type (2'b"10")) [true; true]. Proof ltac:(reflexivity).
+  Goal testAccess (num := 2) (ARRAY {R}) (Const type (1'b"0")) [true]. Proof. reflexivity. Qed.
+  Goal testAccess (num := 2) (ARRAY {R}) (Const type (1'b"1")) [false]. Proof. reflexivity. Qed.
+  
+  Goal testAccess (num := 3) (ARRAY {R; R}) (Const type (2'b"01")) [true; false]. Proof. reflexivity. Qed.
+  Goal testAccess (num := 3) (ARRAY {R; R}) (Const type (2'b"00")) [true; true]. Proof. reflexivity. Qed.
+  Goal testAccess (num := 3) (ARRAY {R; R}) (Const type (2'b"10")) [false; true]. Proof. reflexivity. Qed.
+  Goal testAccess (num := 3) (ARRAY {R; R}) (Const type (2'b"11")) [true; true]. Proof. reflexivity. Qed.
 
   Close Scope kami_expr.
-
+  Opaque wlt_dec.
 End tests.
