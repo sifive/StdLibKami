8bcdd6898a88dfa68119d38f11a5daa065117aa2
Larry Lee <llee454@gmail.com>
2019-12-31 10:59:49 -0800

    Revised the arbiter so that it is parameterized by a list of client records, revised the definition names for clarity.

--- Arbiter/Ifc.v
+++ Arbiter/Ifc.v
@@ -1,38 +1,94 @@
+(*
+  Routes multiple concurrent memory requests from Arbiter clients
+  to a device router that can only accept one request at a time
+  and routes the response callback to the correct client response
+  handler.
+*)
 Require Import Kami.AllNotations.
 Require Import StdLibKami.FreeList.Ifc.
+Require Import List.
+
 Section Arbiter.
   Open Scope kami_expr_scope.
+
+  Record ArbiterClient (reqK respK : Kind)
+    := {
+         arbiterClientTagSz : nat;
+         ArbiterClientTag := Bit arbiterClientTagSz;
+         ArbiterClientReq
+           := STRUCT_TYPE {
+                "tag" :: ArbiterClientTag;
+                "req" :: reqK
+              };
+         ArbiterClientRes
+           := STRUCT_TYPE {
+                "tag"  :: ArbiterClientTag;
+                "resp" :: Maybe respK
+              };
+         arbiterClientHandleRes
+           :  forall {ty}, ty ArbiterClientRes -> ActionT ty Void
+       }.
+
   Class ArbiterParams :=
     {
-      reqK: Kind;
-      respK: Kind;
-      ImmRes: Kind;
-      serverTagSz: nat;
-      arbiterTagNum: nat := pow2 serverTagSz;
-      clientTagSizes: list nat;
-      numClients: nat := List.length clientTagSizes;
+      reqK : Kind;   (* request sent to a memory device - specifically MemDeviceReq *)
+      respK : Kind;  (* data returned by a memory device - specifically Data. *)
+      ImmRes : Kind; (* immediate response from a memory device - specicially Maybe MemErrorPkt. *)
+      transactionTagSz : nat;
+      clients : list (ArbiterClient reqK respK)
     }.
 
   Section withParams.
     Context `{ArbiterParams}.
-    Definition ArbiterTag: Kind := Bit (Nat.log2_up arbiterTagNum).
-    Definition MemReq := STRUCT_TYPE { "tag" :: ArbiterTag;
-                                       "req" :: reqK }.
-    Definition MemResp := STRUCT_TYPE { "tag" :: ArbiterTag;
-                                        "resp" :: Maybe respK }. (* Devices may indicate a failed response. *)
-    Record Arbiter: Type :=
-      {
-        regs: list RegInitT;
-        clientReqGen (memReq: forall {ty},
-                         ty MemReq -> ActionT ty STRUCT_TYPE { "ready" :: Bool;
-                                                               "info" :: ImmRes }):
-          forall (id: Fin.t numClients) {ty}, ty STRUCT_TYPE { "tag" :: Bit (nth_Fin clientTagSizes id);
-                                                               "req" :: reqK } -> ActionT ty STRUCT_TYPE { "ready" :: Bool;
-                                                                                                           "info" :: ImmRes };
-        memCallback (clientCallbacks: forall (id: Fin.t numClients) {ty},
-                        ty STRUCT_TYPE { "tag" :: (Bit (nth_Fin clientTagSizes id));
-                                         "resp" :: Maybe respK } -> ActionT ty Void): forall {ty}, ty MemResp -> ActionT ty Void;
-        arbiterRule: forall {ty}, ActionT ty Void;
-      }.
+
+    Definition arbiterNumClients := length clients.
+
+    Definition arbiterClientReq (clientId : Fin.t arbiterNumClients)
+      := ArbiterClientReq (nth_Fin clients clientId).
+
+    Definition arbiterClientRes (clientId : Fin.t arbiterNumClients)
+      := ArbiterClientRes (nth_Fin clients clientId).
+
+    Definition arbiterNumTransactions := pow2 transactionTagSz.
+
+    Definition ArbiterTransactionTag: Kind := Bit transactionTagSz.
+
+    Definition ArbiterRouterReq
+      := STRUCT_TYPE {
+           "tag" :: ArbiterTransactionTag;
+           "req" :: reqK
+         }.
+
+    Definition ArbiterRouterRes
+      := STRUCT_TYPE {
+           "tag" :: ArbiterTransactionTag;
+           "resp" :: Maybe respK
+         }.
+
+    Definition ArbiterRouterImmRes
+      := STRUCT_TYPE {
+           "ready" :: Bool;
+           "info"  :: ImmRes
+         }.
+
+    Definition ArbiterImmRes := ArbiterRouterImmRes.
+
+    Record Arbiter
+      := {
+           regs : list RegInitT;
+
+           sendReq
+             (routerSendReq 
+               : forall {ty},
+                 ty ArbiterRouterReq ->
+                 ActionT ty ArbiterRouterImmRes)
+             : forall (clientId : Fin.t arbiterNumClients) {ty},
+               ty (arbiterClientReq clientId) ->
+               ActionT ty ArbiterImmRes;
+
+           memCallback : forall {ty}, ty ArbiterRouterRes -> ActionT ty Void;
+
+           arbiterRule : forall {ty}, ActionT ty Void;
+         }.
   End withParams.
 End Arbiter.
--- Arbiter/Impl.v
+++ Arbiter/Impl.v
@@ -1,9 +1,11 @@
 Require Import Kami.All.
 Require Import StdLibKami.Arbiter.Ifc.
 Require Import StdLibKami.FreeList.Ifc.
+Require Import List.
 
 Section ArbiterImpl.
   Context `{ArbiterParams}.
+
   Class ArbiterImplParams :=
     {
       arbiter: string;
@@ -12,96 +14,182 @@ Section ArbiterImpl.
          tags/IDs *)
       alistRead: string;
       alistWrite: string;
-      freelist: @FreeList arbiterTagNum;
+      freelist: @FreeList arbiterNumTransactions;
     }.
+
   Section withParams.
     Context `{ArbiterImplParams}.
+
     Local Open Scope kami_expr.
     Local Open Scope kami_action.
 
-    Definition IdSz: nat := Nat.log2_up numClients.
-    Definition Id: Kind := Bit IdSz.
+    Definition clientIdSz := Nat.log2_up arbiterNumClients.
+    Definition ClientId := Bit clientIdSz.
 
-    Definition ClientTagSz := List.fold_left Nat.max clientTagSizes 0.
-    Definition ClientTag := Bit ClientTagSz.
+    Definition GenericClientTagSz
+      := fold_left Nat.max
+           (map
+             (fun client : ArbiterClient _ _
+               => arbiterClientTagSz client)
+             clients)
+           0.
 
-    Definition IdTag: Kind := STRUCT_TYPE { "id" :: Id;
-                                            "tag" :: ClientTag }.
+    Definition GenericClientTag := Bit GenericClientTagSz.
+
+    Definition ClientIdTag
+      := STRUCT_TYPE {
+           "id"  :: ClientId;
+           "tag" :: GenericClientTag
+         }.
+
     Section withTy.
       Definition nextToAlloc {ty: Kind -> Type} := @nextToAlloc _ freelist ty.
       Definition alloc {ty: Kind -> Type} := @alloc _ freelist ty.
       Definition free {ty: Kind -> Type}:= @free _ freelist ty.
 
       (* Per-client translator request action *)
+
       Open Scope kami_expr_scope.
-      Definition clientReq
-                 (memReq: forall {ty},
-                     ty MemReq ->
-                     ActionT ty STRUCT_TYPE { "ready" :: Bool;
-                                              "info" :: ImmRes })
-                 (id: Fin.t numClients)
-                 (ty: Kind -> Type)
-                 (taggedReq: ty STRUCT_TYPE { "tag" :: Bit (nth_Fin clientTagSizes id);
-                                                                               "req" :: reqK }): ActionT ty STRUCT_TYPE { "ready" :: Bool; "info" :: ImmRes } :=
-        Read arb: Bool <- arbiter;
-        LETA serverTag: Maybe ArbiterTag <- nextToAlloc;
-        LET mRq <- STRUCT { "tag" ::=  #serverTag @% "data";
-                            "req" ::= #taggedReq @% "req" };
-        LET sTagDat <- #serverTag @% "data";
-        If !#arb && #serverTag @% "valid" then (
-          Write arbiter: Bool <- $$true;
-          LETA reqRes <- memReq mRq;
-          If #reqRes @% "ready" then (
-              Call alistWrite(STRUCT { "addr" ::= (#serverTag @% "data");
-                                       "data" ::= STRUCT { "id" ::= $(proj1_sig (Fin.to_nat id));
-                                                           "tag" ::= (ZeroExtendTruncLsb _ (#taggedReq @% "tag") : ClientTag @# ty) }
-                                     }: WriteRq (Nat.log2_up arbiterTagNum) IdTag);
-              LETA _ <- alloc sTagDat ;
-              Retv);
-          Ret #reqRes )
-      else Ret STRUCT { "ready" ::= $$false;
-                        "info" ::= $$(@getDefaultConst ImmRes) } as retVal;
-      Ret #retVal.
 
-    (* What the "real" memory unit will call to respond to the tag
-     translator; This is where the routing of responses to individual
-     clients occurs. *)
+      (*
+        Casts transaction tags stored in the free list into
+        ArbiterTransactionTags.
+      *)
+      Local Definition toTransactionTag
+        (f : nat -> Type)
+        (tag : f (Nat.log2_up arbiterNumTransactions))
+        :  f transactionTagSz
+        := eq_rect
+             (Nat.log2_up arbiterNumTransactions)
+             (fun k => f k)
+             tag
+             transactionTagSz
+             (Nat.log2_up_pow2
+               transactionTagSz
+               (Nat.le_0_l transactionTagSz)).
+
+      Local Definition fromTransactionTag
+        (f : nat -> Type)
+        (tag : f transactionTagSz)
+        :  f (Nat.log2_up arbiterNumTransactions)
+        := eq_rect_r f tag
+             (Nat.log2_up_pow2
+               transactionTagSz
+               (Nat.le_0_l transactionTagSz)).
+
+      Definition sendReq
+        (routerSendReq
+          : forall {ty},
+            ty ArbiterRouterReq ->
+            ActionT ty ArbiterRouterImmRes)
+        (clientId : Fin.t arbiterNumClients)
+        (ty : Kind -> Type)
+        (clientReq : ty (arbiterClientReq clientId))
+        :  ActionT ty ArbiterImmRes
+        := Read busy : Bool <- arbiter;
+           LETA transactionTag
+             :  Maybe ArbiterTransactionTag
+             <- toTransactionTag
+                  (fun n => ActionT ty (Maybe (Bit n)))
+                  nextToAlloc;
+           If !#busy && #transactionTag @% "valid"
+             then
+               Write arbiter : Bool <- $$true;
+               LET routerReq
+                 :  ArbiterRouterReq
+                 <- STRUCT {
+                      "tag" ::= #transactionTag @% "data";
+                      "req" ::= #clientReq @% "req"
+                    };
+               LETA routerImmRes
+                 :  ArbiterRouterImmRes
+                 <- routerSendReq routerReq;
+               If #routerImmRes @% "ready"
+                 then
+                   LET clientIdTag
+                     :  ClientIdTag
+                     <- STRUCT {
+                          "id"  ::= $(proj1_sig (Fin.to_nat clientId));
+                          "tag" ::= ZeroExtendTruncLsb GenericClientTagSz (#clientReq @% "tag")
+                        };
+                   LET transaction
+                     :  WriteRq transactionTagSz ClientIdTag
+                     <- STRUCT {
+                          "addr" ::= #transactionTag @% "data";
+                          "data" ::= #clientIdTag
+                        };
+                   Call alistWrite (#transaction : WriteRq transactionTagSz ClientIdTag);
+                   LET transactionTagData
+                     :  ArbiterTransactionTag
+                     <- #transactionTag @% "data";
+                   alloc
+                     (fromTransactionTag
+                       (fun n => ty (Bit n))
+                       transactionTagData);
+               Ret #routerImmRes
+             else
+               Ret $$(getDefaultConst ArbiterImmRes)
+             as result;
+           Ret #result.
+
+      (*
+        What the "real" memory unit will call to respond to the tag
+        translator; This is where the routing of responses to individual
+        clients occurs.
+      *)
       Definition memCallback
-                 (clientCallbacks: forall (id: Fin.t numClients) {ty},
-                     ty STRUCT_TYPE { "tag" :: (Bit (nth_Fin clientTagSizes id));
-                                      "resp" :: Maybe respK } -> ActionT ty Void)
-                 (ty: Kind -> Type)
-                 (resp: ty MemResp): ActionT ty Void :=
-        LET sTag: ArbiterTag <- #resp @% "tag";
-        Call idtag: IdTag <- alistRead(#sTag: ArbiterTag);
-        LETA _ <- free sTag;
-        LET respId: Id <- #idtag @% "id";
-        LET respTag: ClientTag <- #idtag @% "tag";
-        GatherActions (List.map (fun (id: Fin.t numClients) => 
-                                   LET clientTag: Bit (nth_Fin clientTagSizes id) <- ZeroExtendTruncLsb _ (#resp @% "tag");
-                                     LET respDat <- #resp @% "resp";
-                                     LET taggedResp <- STRUCT { "tag" ::= #clientTag;
-                                                                "resp" ::= #respDat };
-                                     If $(proj1_sig (Fin.to_nat id)) == #respId then (
-                                     LETA _ <- (clientCallbacks id) taggedResp;
-                                       Retv
-                                   ); Retv
-                                )
-                                (getFins numClients)) as _; Retv.
+        (ty: Kind -> Type)
+        (routerRes: ty ArbiterRouterRes)
+        :  ActionT ty Void
+        := LET transactionTag
+             :  ArbiterTransactionTag
+             <- #routerRes @% "tag";
+           Call clientIdTag
+             :  ClientIdTag
+             <- alistRead (#transactionTag: ArbiterTransactionTag);
+           LETA _
+             <- free
+                  (fromTransactionTag
+                    (fun n => ty (Bit n))
+                    transactionTag);
+           GatherActions
+             (map
+               (fun (clientId: Fin.t arbiterNumClients)
+                 => let client
+                      :  ArbiterClient _ _
+                      := nth_Fin clients clientId in 
+                    If $(proj1_sig (Fin.to_nat clientId)) == (#clientIdTag @% "id")
+                      then
+                        LET clientRes
+                          :  ArbiterClientRes client
+                          <- STRUCT {
+                               "tag"  ::= ZeroExtendTruncLsb (arbiterClientTagSz client) (#routerRes @% "tag");
+                               "resp" ::= #routerRes @% "resp"
+                             };
+                        arbiterClientHandleRes client clientRes;
+                    Retv)
+               (getFins arbiterNumClients))
+             as _;
+           Retv.
 
-      Definition arbiterRule ty: ActionT ty Void :=
-        Write arbiter: Bool <- $$false;
-        Retv.
+      (* TODO: LLEE does this make sense? *)
+      Definition arbiterRule ty : ActionT ty Void
+        := Write arbiter : Bool <- $$false;
+           Retv.
+
     End withTy.
 
     Open Scope kami_scope.
     Open Scope kami_expr_scope.
+
     Definition regs: list RegInitT := makeModule_regs ( Register arbiter: Bool <- false  ) ++ (FreeList.Ifc.regs freelist).
 
-    Definition arbiterImpl := Build_Arbiter
-                                regs
-                                clientReq
-                                memCallback
-                                arbiterRule.
+    Definition arbiterImpl
+      := Build_Arbiter
+           regs
+           sendReq
+           memCallback
+           arbiterRule.
+
   End withParams.
 End ArbiterImpl.
