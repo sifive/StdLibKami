aa95f5bba21dd867370990c980e3a0a3422a3769
Murali Vijayaraghavan <vmurali@sifive.com>
2020-01-23 19:39:51 -0800

    fixed Prefetcher.

--- Prefetcher/Ifc.v
+++ Prefetcher/Ifc.v
@@ -1,18 +1,49 @@
 Require Import Kami.AllNotations.
 Require Import StdLibKami.Fifo.Ifc.
-Require Import StdLibKami.Prefetcher.FifoTop.Ifc.
 
 Class PrefetcherParams :=
-  { PrivMode: Kind;
-    PAddrSz: nat }.
-                           
+  { PrivMode : Kind;
+    PAddrSz : nat;
+    VAddrSz : nat;
+    CompInstSz : nat;
+    ImmRes : Kind;
+    isCompressed: forall ty, Bit CompInstSz @# ty -> Bool @# ty;
+    isErr: forall ty, ImmRes @# ty -> Bool @# ty
+  }.
 
-Section prefetcher.
-  Context `{fifoTopParams : FifoTopParams}.
-  Context `{PrefetcherParams}.
+Section Prefetcher.
+  Context `{prefetcherParams: PrefetcherParams}.
 
-  Local Definition PAddr := Bit PAddrSz.
+  Local Definition PAddr    := Bit PAddrSz.
+  Local Definition VAddr    := Bit VAddrSz.
+  Local Definition CompInst := Bit CompInstSz.
+  Local Definition InstSz   := CompInstSz + CompInstSz.
+  Local Definition Inst     := Bit InstSz.
 
+  Definition ShortVAddrSz := (VAddrSz - 2)%nat.
+
+  Definition ShortVAddr := Bit ShortVAddrSz.
+
+  Definition toFullVAddr {ty} (short: ShortVAddr @# ty): VAddr @# ty := ZeroExtendTruncLsb _ ({< short, $$(natToWord 2 0) >})%kami_expr.
+  
+  Definition toShortVAddr {ty} (long: VAddr @# ty): ShortVAddr @# ty := ZeroExtendTruncMsb _ (long)%kami_expr.
+    
+  Definition PrefetcherFifoEntry
+    := STRUCT_TYPE {
+         "vaddr" :: ShortVAddr;
+         "info"  :: ImmRes;  
+         "inst"  :: Maybe Inst
+       }.
+
+  Definition TopEntry: Kind
+    := STRUCT_TYPE {
+         "vaddr" :: ShortVAddr;
+         "info"  :: ImmRes;
+         "noErr" :: Bool;
+         "upper" :: Maybe CompInst;
+         "lower" :: Maybe CompInst
+       }.
+  
   Definition PrefetcherReq
     := STRUCT_TYPE {
          "mode"  :: PrivMode;
@@ -27,20 +58,27 @@ Section prefetcher.
          "inst"  :: Maybe Inst
        }.
 
+  (* if inst contains a compressed instruction the upper 16 bit contain arbitrary data. *)
+  Definition DeqRes
+    := STRUCT_TYPE {
+         "vaddr" :: VAddr;
+         "info"  :: ImmRes;
+         "noErr" :: Bool;
+         "inst"  :: Inst 
+       }.
+
   Class Prefetcher: Type :=
     {
       regs: list RegInitT;
       regFiles : list RegFileBase;
 
-      (*
-        Returns the address that needs to be fetched to retrieve the
-        upper 16 bits of the current instruction's 32 bit word.
-      *)
-      getIsCompleting: forall {ty}, ActionT ty (Maybe VAddr);
-      resetCompleting: forall {ty}, ActionT ty Void;
       isFull: forall {ty}, ActionT ty Bool;
-      memCallback: forall {ty}, ty PrefetcherRes -> ActionT ty Void;
-      fetchInstruction: forall {ty}, ActionT ty DeqRes;
       doPrefetch ty (memReq: ty PrefetcherReq -> ActionT ty Bool): ty PrefetcherReq -> ActionT ty Bool;
+      memCallback: forall {ty}, ty PrefetcherRes -> ActionT ty Void;
+      fetchInstruction: forall {ty}, ActionT ty (Maybe DeqRes);
+      isNotComplete: forall {ty}, ActionT ty Bool;
+      clearTop: forall {ty}, ActionT ty Void;
+      notCompleteDeq: forall {ty}, ActionT ty Void;
+      transfer: forall {ty}, ActionT ty Void;
     }.
-End prefetcher.
+End Prefetcher.
