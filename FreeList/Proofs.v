b586e065e6e0653a673cbbbb4f7ab47f16699851
Anthony James Machado <39344451+tjmach@users.noreply.github.com>
2020-02-19 19:27:43 -0800

    Removed KamiCorrectness.  Comments in FreeList/Proofs.v (#52)

--- FreeList/Proofs.v
+++ FreeList/Proofs.v
@@ -1,9 +1,125 @@
+(* Inductive Expr := *)
+(* | One : Expr *)
+(* | Mul : Expr -> Expr -> Expr *)
+(* | Add : Expr -> Expr -> Expr *)
+(* | Opp : Expr -> Expr. *)
+
+(* (* Expr is defined *) *)
+(* (* Expr_rect is defined *) *)
+(* (* Expr_ind is defined *) *)
+(* (* Expr_rec is defined *) *)
+
+(* Declare Custom Entry expr. *)
+(* Notation "[ e ]" := e (e custom expr at level 2). *)
+
+(* (* Setting notation at level 0. *) *)
+
+(* Notation " 1 " := One (in custom expr at level 0). *)
+(* Notation "x * y" := (Mul x y) (in custom expr at level 1, left associativity). *)
+(* Notation "x + y" := (Add x y) (in custom expr at level 2, left associativity). *)
+(* Notation "{| x , y |}" := (Mul x y) (in custom expr at level 2, left associativity). *)
+
+
+(* Notation "( x )" := x (in custom expr, x at level 2). *)
+
+(* (* Setting notation at level 0. *) *)
+
+(* Notation "- y" := (Opp y) (in custom expr at level 1). *)
+
+(* Notation "x - y" := (Add x (Opp y)) (in custom expr at level 2). *)
+
+(* (* Setting notation at level 0. *) *)
+
+(* Notation "{ x }" := x (in custom expr, x constr). *)
+
+(* (* Setting notation at level 0. *) *)
+
+(* Notation "x" := x (in custom expr at level 0, x ident). *)
+(* Axiom f : nat -> Expr. *)
+
+(* (* f is declared *) *)
+
+(* Unset Printing Notations. *)
+(* Check fun x y => [{|x, y |}]. *)
+
+(* Check fun x y z => [1 + y * z + {f (x + 1)}]. *)
+(* Unset Printing Notations. *)
+(* Check fun x y z => [1 + y * z + {f (x + 1)}]. *)
+(* Check fun x y => [x + y * x + y]. *)
+(* Check fun x y z => [- 1 - x + y * z]. *)
+
+(* Set Printing Notations. *)
+(* Check (fun x y z => (Add (Add (Add (Opp One) (Opp (Mul y z))) (f x))) (f 1 )). *)
+
+(* Definition testing_notations x y z := *)
+(*   [x + y * z + - 1]. *)
+
+(* Print testing_notations. *)
+
+(* Inductive BinOps := *)
+(* | BinEq : Expr -> Expr -> BinOps *)
+(* | BinLt : Expr -> Expr -> BinOps *)
+(* | BinGt : Expr -> Expr -> BinOps. *)
+
+(* Declare Custom Entry binexpr. *)
+(* Notation "[| b |]" := b (b custom binexpr at level 4, no associativity). *)
+
+(* (* Notation "x = y" := (BinEq x y) (in custom binexpr at level 4, no associativity, x custom expr at level 2, y custom expr at level 2). *) *)
+(* (* Notation "x < y" := (BinLt x y) (in custom binexpr at level 4, no associativity, x custom expr at level 2, y custom expr at level 2). *) *)
+(* (* Notation "x > y" := (BinGt x y) (in custom binexpr at level 4, no associativity, x custom expr at level 2, y custom expr at level 2). *) *)
+
+(* Notation "x = y" := (BinEq x y) (in custom binexpr at level 4, no associativity). *)
+(* Notation "x < y" := (BinLt x y) (in custom binexpr at level 4, no associativity). *)
+(* Notation "x > y" := (BinGt x y) (in custom binexpr at level 4, no associativity). *)
+
+(* Notation " x " := x (in custom binexpr at level 0, x custom expr at level 2). *)
+
+(* Check fun w x y z => [|x + y > y * z + {f w}|]. *)
+(* Check fun x y => [| x + y = 1 - x |]. *)
+(* Check fun x y z => [|x + y * z - 1|]. *)
+
+
+(* (* fun (x : nat) (y z : Expr) => [~ 1 + y z + {f x}] *) *)
+(* (*      : nat -> Expr -> Expr -> Expr *) *)
+
+(* Unset Printing Notations. *)
+(* Check fun x y z => [-1 + y * z + {f x}]. *)
+
+(* (* fun (x : nat) (y z : Expr) => Add (Add (Opp One) (Mul y z)) (f x) *) *)
+(* (*      : forall (_ : nat) (_ : Expr) (_ : Expr), Expr *) *)
+
+(* Set Printing Notations. *)
+(* Check fun e => match e with *)
+(* | [1 + 1] => [|1|] *)
+(* | [x * y + z] => [x + y * z] *)
+(* | y => [y + e] *)
+(* end. *)
+
+(* (* fun e : Expr => *) *)
+(* (* match e with *) *)
+(* (* | [1 + 1] => [1] *) *)
+(* (* | [x y + z] => [x + y z] *) *)
+(* (* | _ => [e + e] *) *)
+(* (* end *) *)
+(* (* : Expr -> Expr *) *)
+
+
+(* Check fun x y z => [~1 + y z + {f x}]. *)
+
+(* Definition foo := [~1 + {f 1} 1]. *)
+(* Print foo. *)
+(* Print Custom Grammar expr. *)
+
+
+
 Require Import Kami.All.
 Require Import StdLibKami.FreeList.Spec.
 Require Import StdLibKami.FreeList.Ifc.
 Require Import StdLibKami.FreeList.Array.
 Require Import Coq.Logic.EqdepFacts.
-Require Import StdLibKami.KamiCorrectness.
+Require Import Kami.GallinaModules.Relations.
+Require Import Kami.GallinaModules.AuxTactics.
+Require Import Kami.GallinaModules.AuxLemmas.
 
 Record FreeListCorrect {FLParams} (imp spec: @FreeList FLParams): Type :=
   {
@@ -38,6 +154,8 @@ Section Proofs.
     match goal with
     | [ H : myArrayFreeListR _ _ |- _] => destruct H
     end.
+  
+  
 
   (* Goal FreeListCorrect arrFreeList spFreeList. *)
   (*   econstructor 1 with (freeListR := myArrayFreeListR) *)
@@ -50,8 +168,18 @@ Section Proofs.
   (*     ; try (destruct LocalReg; inv LocalRegVal) *)
   (*     ; unfold nextToAlloc, alloc, free, arrayFreeList, specFreeList, arrFreeList, spFreeList, *)
   (*       arrayFreeList, specFreeList, Array.nextToAlloc, Spec.nextToAlloc in *. *)
-  (*   unfold in *. *)
-  (*   unfold arrayFreeList in *. *)
-  (*   unfold Array.nextToAlloc in *. *)
-
+  (*   - hyp_consumer1. *)
+  (*     + goal_consumer. *)
+  (*       my_simpl_solver. *)
+  (*       goal_consumer. *)
+  (*       my_simpl_solver. *)
+  (*       repeat goal_consumer. *)
+  (*       3 : { reflexivity. *)
+  (*       } *)
+  (*       3 : auto. *)
+  (*       3 : auto. *)
+  (*       3 : auto. *)
+  (*       unfold Array.arrayRegName. *)
+  (*       unfold Spec.arrayRegName. *)
+  (*       auto. *)
 End Proofs.
