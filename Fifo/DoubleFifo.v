a9ddc6ff4f4b97639c3255a82ceffa0d460b89e3
Anthony Machado <tjmach@sifive.com>
2020-04-06 09:38:11 -0700

    Some changes to how the generic proof works.

--- Fifo/DoubleFifo.v
+++ Fifo/DoubleFifo.v
@@ -6,90 +6,112 @@ Require Import StdLibKami.Fifo.Impl.
 Section DoubleFifo.
   
   Context {ifcParams : Ifc.Params}.
+  Variable sizeL sizeR : nat.
   Local Definition ifcParamsL :=
-    Ifc.Build_Params (append name "L") k ((@size ifcParams)/2).
+    Ifc.Build_Params (append name "L") k sizeL.
   Local Definition ifcParamsR :=
-    Ifc.Build_Params (append name "R") k ((@size ifcParams)/2).
+    Ifc.Build_Params (append name "R") k sizeR.
 
-  Class Params := { sizePow2 : Nat.pow 2 (Nat.log2_up size) = size;
+  Class Params := { sizeSum : size = sizeL + sizeR;
                     sizeGt1 : 1 < size;
-                    fifoL : @Fifo.Ifc.Ifc ifcParamsL;
-                    fifoR : @Fifo.Ifc.Ifc ifcParamsR }.
+                    Lfifo : @Fifo.Ifc.Ifc ifcParamsL;
+                    Rfifo : @Fifo.Ifc.Ifc ifcParamsR }.
 
   Context {params: Params}.
+  
+  (* Local Lemma lgSize_double : *)
+  (*   ((@lgSize ifcParamsL) + 1)%nat = @lgSize ifcParams. *)
+  (* Proof. *)
+  (*   unfold Ifc.lgSize, Ifc.size. *)
+  (*   cbn [ifcParamsL]. *)
+  (*   rewrite Nat.add_comm. *)
+  (*   rewrite <- Nat.log2_up_mul_pow2; try lia. *)
+  (*   - f_equal. *)
+  (*     rewrite Nat.pow_1_r. *)
+  (*     assert (Ifc.size mod 2 = 0) as P. *)
+  (*     + rewrite Nat.mod_divide; [|lia]. *)
+  (*       unfold Nat.divide. *)
+  (*       specialize sizePow2 as P. *)
+  (*       specialize sizeGt1 as P0. *)
+  (*       rewrite <- P. *)
+  (*       destruct (Nat.log2_up Ifc.size). *)
+  (*       * exfalso; rewrite <- P in P0; simpl in P0; lia. *)
+  (*       * cbn [Nat.pow]. *)
+  (*         exists (2^n). *)
+  (*         apply Nat.mul_comm. *)
+  (*     + rewrite Nat.mul_comm. *)
+  (*       rewrite mul_div_exact; auto; try lia. *)
+  (*   - specialize sizePow2 as P. *)
+  (*     specialize sizeGt1 as P0. *)
+  (*     destruct (Nat.log2_up size). *)
+  (*     + exfalso; rewrite <- P in P0; simpl in P0; lia. *)
+  (*     + rewrite <- P; cbn[Nat.pow]. *)
+  (*       rewrite mul_div_undo; try lia. *)
+  (*       specialize (Nat.pow_nonzero 2 n ltac:(lia)) as P1. *)
+  (*       lia. *)
+  (* Qed. *)
 
-  Local Lemma lgSize_double :
-    ((@lgSize ifcParamsL) + 1)%nat = @lgSize ifcParams.
+  Local Lemma lgSize_sumL :
+    (@lgSize ifcParamsL) + (lgSize - (@lgSize ifcParamsL)) = lgSize.
   Proof.
-    unfold Ifc.lgSize, Ifc.size.
-    cbn [ifcParamsL].
-    rewrite Nat.add_comm.
-    rewrite <- Nat.log2_up_mul_pow2; try lia.
-    - f_equal.
-      rewrite Nat.pow_1_r.
-      assert (Ifc.size mod 2 = 0) as P.
-      + rewrite Nat.mod_divide; [|lia].
-        unfold Nat.divide.
-        specialize sizePow2 as P.
-        specialize sizeGt1 as P0.
-        rewrite <- P.
-        destruct (Nat.log2_up Ifc.size).
-        * exfalso; rewrite <- P in P0; simpl in P0; lia.
-        * cbn [Nat.pow].
-          exists (2^n).
-          apply Nat.mul_comm.
-      + rewrite Nat.mul_comm.
-        rewrite mul_div_exact; auto; try lia.
-    - specialize sizePow2 as P.
-      specialize sizeGt1 as P0.
-      destruct (Nat.log2_up size).
-      + exfalso; rewrite <- P in P0; simpl in P0; lia.
-      + rewrite <- P; cbn[Nat.pow].
-        rewrite mul_div_undo; try lia.
-        specialize (Nat.pow_nonzero 2 n ltac:(lia)) as P1.
-        lia.
+    symmetry.
+    apply le_plus_minus.
+    unfold lgSize, ifcParamsL.
+    rewrite sizeSum; unfold size.
+    apply Nat.log2_up_le_mono; lia.
+  Qed.
+
+  Local Lemma lgSize_sumR :
+    (@lgSize ifcParamsR) + (lgSize - (@lgSize ifcParamsR)) = lgSize.
+  Proof.
+    symmetry.
+    apply le_plus_minus.
+    unfold lgSize, ifcParamsR.
+    rewrite sizeSum; unfold size.
+    apply Nat.log2_up_le_mono; lia.
   Qed.
   
   Local Open Scope kami_expr.
   Local Open Scope kami_action.
 
-  Local Definition isEmpty ty: ActionT ty Bool := isEmpty fifoL.
+  Local Definition isEmpty ty: ActionT ty Bool := isEmpty Lfifo.
 
-  Local Definition isFull ty: ActionT ty Bool := isFull fifoR.
+  Local Definition isFull ty: ActionT ty Bool := isFull Rfifo.
   
   Local Definition numFree ty: ActionT ty (Bit (@lgSize ifcParams)) :=
-    LETA numL: Bit (@lgSize ifcParamsL) <- (Ifc.numFree fifoL);
-    LETA numR: Bit (@lgSize ifcParamsR) <- (Ifc.numFree fifoR);
-    Ret (castBits lgSize_double ((ZeroExtend 1 #numL) + (ZeroExtend 1 #numR))).
+    LETA numL: Bit (@lgSize ifcParamsL) <- (Ifc.numFree Lfifo);
+    LETA numR: Bit (@lgSize ifcParamsR) <- (Ifc.numFree Rfifo);
+    Ret (castBits lgSize_sumL (ZeroExtend (lgSize - (@lgSize ifcParamsL)) #numL)
+        + castBits lgSize_sumR (ZeroExtend (lgSize - (@lgSize ifcParamsR)) #numR)).
 
-  Local Definition first ty: ActionT ty (Maybe k) := first fifoR.
+  Local Definition first ty: ActionT ty (Maybe k) := first Rfifo.
 
-  Local Definition deq ty: ActionT ty (Maybe k) := deq fifoR.
+  Local Definition deq ty: ActionT ty (Maybe k) := deq Rfifo.
 
-  Local Definition enq ty (new: ty k): ActionT ty Bool := enq fifoL new.
+  Local Definition enq ty (new: ty k): ActionT ty Bool := enq Lfifo new.
 
   Local Definition fillRule ty : ActionT ty Void :=
-    LETA fullR: Bool <- (Ifc.isFull fifoR);
-    LETA emptyL: Bool <- (Ifc.isEmpty fifoL);
+    LETA fullR: Bool <- (Ifc.isFull Rfifo);
+    LETA emptyL: Bool <- (Ifc.isEmpty Lfifo);
     If !(#fullR && #emptyL) then (
-      LETA val: Maybe k <- (Ifc.deq fifoL);
+      LETA val: Maybe k <- (Ifc.deq Lfifo);
       LET data <- #val @% "data";
-      LETA _ : Bool <- (Ifc.enq fifoR data);
+      LETA _ : Bool <- (Ifc.enq Rfifo data);
       Retv
       );
     Retv.
                     
    Local Definition flush ty: ActionT ty Void :=
-     LETA _ <- (Ifc.flush fifoR);
-     LETA _ <- (Ifc.flush fifoL);
+     LETA _ <- (Ifc.flush Rfifo);
+     LETA _ <- (Ifc.flush Lfifo);
      Retv.
 
-   Local Definition regs: list RegInitT := (Ifc.regs fifoL) ++ (Ifc.regs fifoR).
+   Local Definition regs: list RegInitT := (Ifc.regs Lfifo) ++ (Ifc.regs Rfifo).
 
   Definition impl: Ifc :=
     {|
       Ifc.regs := regs;
-      Ifc.regFiles := (Ifc.regFiles fifoL) ++ (Ifc.regFiles fifoR);
+      Ifc.regFiles := (Ifc.regFiles Lfifo) ++ (Ifc.regFiles Rfifo);
       Ifc.isEmpty := isEmpty;
       Ifc.isFull := isFull;
       Ifc.numFree := numFree;
--- Fifo/GenericProofs.v
+++ Fifo/GenericProofs.v
@@ -8,27 +8,84 @@ Require Import StdLibKami.Fifo.Ifc.
 Require Import StdLibKami.Fifo.GenericSpec.
 Require Import StdLibKami.Fifo.DoubleFifo.
 Require Import StdLibKami.Fifo.CorrectDef.
+Require Import StdLibKami.Fifo.Proofs2.
 
+
+Definition valsToSnd {k : Kind} (l : list (type k)) :=
+  map (fun x => existT (fullType type) (SyntaxKind k)
+                       (nth_default (evalConstT Default) l x)) (seq 0 (length l)).
 
 Section Proofs.
-  Context {ifcParams' : Fifo.Ifc.Params}.
-  Context {dblParams' : Fifo.DoubleFifo.Params}.
-  
-  Local Definition fifoImpl1 := @Fifo.DoubleFifo.impl ifcParams' dblParams'.
-  Local Definition fifoImpl2 := @Fifo.GenericSpec.spec ifcParams'.
+  Context {ifcParams : Fifo.Ifc.Params}.
+  Context {dblParams : Fifo.DoubleFifo.Params}.
 
-  Record myFifoImpl1R  (implRegs : RegsT) (fifo1_bval : bool) (fifo1_dval : type k)
-         (o_i o_s : RegsT) : Prop :=
+  Local Definition fifoImpl := @Fifo.DoubleFifo.impl ifcParams dblParams.
+  Local Definition fifoSpec := @Fifo.GenericSpec.spec ifcParams.
+  Local Definition fifoSpecL := @Fifo.GenericSpec.spec (@Fifo.DoubleFifo.ifcParamsL ifcParams).
+  Local Definition fifoSpecR := @Fifo.GenericSpec.spec (@Fifo.DoubleFifo.ifcParamsR ifcParams).
+  
+  Variable HCorrectL : FifoCorrect Lfifo fifoSpecL.
+  Variable HCorrectR : FifoCorrect Rfifo fifoSpecR.
+  
+  Record myGenFifoR  (o_i o_s : RegsT) : Prop :=
     {
-      implRegVal : implRegs = [(Fifo1.validRegName,
-                                existT _ (SyntaxKind Bool) fifo1_bval);
-                              (Fifo1.dataRegName,
-                               existT _ (SyntaxKind k) fifo1_dval)];
-      Ho_iCorrect1 : o_i = implRegs;
-      Ho_sCorrect1 : o_s = implRegs;
+      o_i1 : RegsT;
+      o_i2 : RegsT;
+      o_s1 : RegsT;
+      o_s2 : RegsT;
+      implRegsL : RegsT;
+      implRegsR : RegsT;
+      implRegValL : list (type k);
+      implRegValR : list (type k);
+      specRegVals : list (type k);
+      nonDetEmpVal : bool;
+      nonDetEmpValL : bool;
+      lenVal : word lgSize;
+      lenValL : word (@lgSize (@Fifo.DoubleFifo.ifcParamsL ifcParams));
+      lenValR : word (@lgSize (@Fifo.DoubleFifo.ifcParamsR ifcParams));
+      HimplRegValL : specRegVals = implRegValL ++ implRegValR;
+      HnonDetEmpVal : nonDetEmpVal = nonDetEmpValL;
+      Ho_sCorrect : o_s =
+                    [(GenericSpec.nonDetEmptyName, existT _ (SyntaxKind Bool) nonDetEmpVal);
+                    (GenericSpec.listName, existT _ (GenericSpec.nlist type) specRegVals);
+                    (GenericSpec.nonDetLenName, existT _ (SyntaxKind (Bit lgSize)) lenVal)];
+      Ho_s1Correct : o_s1 =
+                    [((@GenericSpec.nonDetEmptyName (@Fifo.DoubleFifo.ifcParamsL ifcParams)), existT _ (SyntaxKind Bool) nonDetEmpVal);
+                    ((@GenericSpec.listName (@Fifo.DoubleFifo.ifcParamsL ifcParams)), existT _ (GenericSpec.nlist type) specRegVals);
+                    ((@GenericSpec.nonDetLenName (@Fifo.DoubleFifo.ifcParamsL ifcParams)), existT _ (SyntaxKind (Bit lgSize)) lenVal)];
+      Ho_iApp : o_i = o_i1 ++ o_i2;
+      HRegs1 : getKindAttr o_i1 = (fifoRegs HCorrectL);
+      HRegs2 : getKindAttr o_i2 = (fifoRegs HCorrectR);
+      HfifoR1 : (fifoR HCorrectL) o_i1 o_s1;
+      HfifoR2 : (fifoR HCorrectR) o_i2 o_s2;
       Ho_iNoDup1 : NoDup (map fst o_i);
       Ho_sNoDup1 : NoDup (map fst o_s);
     }.
 
+  Ltac Record_destruct :=
+    match goal with
+    | [H : myGenFifoR _ _ |- _] =>
+      destruct H
+    end.
+
+  Goal FifoCorrect fifoImpl fifoSpec.
+    destruct HCorrectL, HCorrectR.
+    econstructor 1 with (fifoR := myGenFifoR)
+                        (fifoRegs := fifoRegs ++ fifoRegs0).
+    all : unfold EffectfulRelation, EffectlessRelation,
+          fifoImpl, fifoSpec, impl, spec,
+          isEmpty, isFull, numFree, first, deq, enq, flush,
+          GenericSpec.isEmpty, GenericSpec.isFull, GenericSpec.numFree, GenericSpec.first,
+          GenericSpec.deq, GenericSpec.enq, GenericSpec.flush,
+          DoubleFifo.isEmpty, DoubleFifo.isFull, DoubleFifo.numFree, DoubleFifo.first,
+          DoubleFifo.deq, DoubleFifo.enq, DoubleFifo.flush.
+    all: intros; try Record_destruct; destruct HCorrectL, HCorrectR;
+      cbn [CorrectDef.fifoRegs CorrectDef.fifoR] in *.
+    - unfold fifoSpecL, spec, GenericSpec.isEmpty, GenericSpec.numFree in *; cbn [isEmpty] in *.
+      hyp_consumer.
+      goal_consumer1.
+      unfold GenericSpec.nlist, DoubleFifo.ifcParamsL, k in x1.
+      rewrite (Eqdep.EqdepTheory.UIP_refl _ _ x1).
+  Admitted.
   
 End Proofs.
