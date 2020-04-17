0497c0bb8a35cea199d4d7aea5e06944b71452c4
Anthony James Machado <39344451+tjmach@users.noreply.github.com>
2020-04-16 17:10:34 -0700

    Fifo proofs (#68)
    * More admits removed.  Possible circular dep problem.
    
    * Trying to finish this proof, currently experiencing problems with
    correctness relations and existentials.
    
    * Have to fix issues with clobbering hyps.

--- Fifo/DoubleFifo.v
+++ Fifo/DoubleFifo.v
@@ -6,11 +6,6 @@ Require Import StdLibKami.Fifo.Impl.
 Section DoubleFifo.
   
   Context {ifcParams : Ifc.Params}.
-  (* Variable sizeL sizeR : nat. *)
-  (* Local Definition ifcParamsL := *)
-  (*   Ifc.Build_Params (append name "L") k sizeL. *)
-  (* Local Definition ifcParamsR := *)
-  (*   Ifc.Build_Params (append name "R") k sizeR. *)
 
   Class Params := { sizeL : nat;
                     sizeR : nat;
@@ -33,28 +28,6 @@ Section DoubleFifo.
                                                             (@k ifcParams)
                                                             sizeR).
   
-  Local Lemma lgSize_sumL :
-    ((@lgSize ifcParamsL) + 1)
-    + ((lgSize + 1) - ((@lgSize ifcParamsL) + 1)) = (lgSize + 1).
-  Proof.
-    symmetry.
-    apply le_plus_minus.
-    unfold lgSize.
-    apply plus_le_compat_r, Nat.log2_up_le_mono.
-    rewrite sizeSum; unfold size; lia.
-  Qed.
-
-  Local Lemma lgSize_sumR :
-    ((@lgSize ifcParamsR) + 1)
-    + ((lgSize + 1) - ((@lgSize ifcParamsR) + 1)) = (lgSize + 1).
-  Proof.
-    symmetry.
-    apply le_plus_minus.
-    unfold lgSize.
-    apply plus_le_compat_r, Nat.log2_up_le_mono.
-    rewrite sizeSum; unfold size; lia.
-  Qed.
-  
   Local Open Scope kami_expr.
   Local Open Scope kami_action.
 
@@ -64,10 +37,7 @@ Section DoubleFifo.
   
   Local Definition numFree ty: ActionT ty (Bit ((@lgSize ifcParams) + 1)) :=
     LETA numL : Bit (lgSize + 1) <- (Ifc.numFree Lfifo);
-    LETA numR : Bit (lgSize + 1) <- (Ifc.numFree Rfifo);
-    Ret (IF (#numL < $(sizeL))
-         then (castBits lgSize_sumL (ZeroExtend _ #numL))
-         else ($sizeL + castBits lgSize_sumR (ZeroExtend _ #numR))).
+    Ret (ZeroExtendTruncLsb _ #numL).
   
   Local Definition first ty: ActionT ty (Maybe k) := first Rfifo.
 
